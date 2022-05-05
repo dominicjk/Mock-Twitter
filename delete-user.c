d8747012624c2f0ad9b60ecad074306eb405c292
dominicjk <102314055+dominicjk@users.noreply.github.com>
2022-05-05 09:49:17 +0100

    update delete user

--- delete-user.c
+++ delete-user.c
@@ -1,5 +1,5 @@
 #include "delete-user.h"
-
+#include "unfollow-user.h"
 //DELETE ACCOUNT FUNCTION
 /*an outer loop will iterate over all the users in the system. When the user intended for deletion is
   matched, then each subsequent user is shifted one place to the left of the user array, which overwrites
@@ -9,22 +9,33 @@
   iterate over each user in the main function, the last user will be skipped.
 */
 void deleteuser(user* a, twitter *system) {
+
+    printf("%s's account has been deleted\n", a -> username);
+    printf("next user is automatically activated\n");
+
+    for (int i = 0; i < system -> num_users; i++) {
+        for (int j = 0; j <= system -> users[i] .num_following; j++) {
+            if (strcmp(a -> username, system -> users[i].following[j]) == 0) {
+                unfollow(&system -> users[i], a);
+            }
+        }
+    }
+
     for (int i = 0; i < system -> num_users; i++) {
         if (strcmp(a ->username, system -> users[i].username) == 0) {
 
-            //inner loop will shift each subsequent user to the left.
             for(int j = i; j < system -> num_users; j++){
+                //strcpy(system -> users[j].username, system -> users[j + 1].username);
 
                 //if deleted user is not last (if it is last just decrement the number of users)
                 if (i < system -> num_users - 1) {
                     struct user *a = &system->users[j];
                     struct user *b = &system->users[j + 1];
-
-                    *a = *b; //overwrite deleted user
+                    *a = *b;
                 }
             }
         }
     }
+    system -> num_users--;
 
-    system -> num_users--; //decrement total user count
 }
