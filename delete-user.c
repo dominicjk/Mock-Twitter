d0aa9775a0f29689e23f93219878112589eb335a
dominicjk <102314055+dominicjk@users.noreply.github.com>
2022-05-05 10:31:57 +0100

    Update delete-user.c

--- delete-user.c
+++ delete-user.c
@@ -1,7 +1,10 @@
 #include "delete-user.h"
 #include "unfollow-user.h"
 //DELETE ACCOUNT FUNCTION
-/*an outer loop will iterate over all the users in the system. When the user intended for deletion is
+/*The first part of the function will iterate over every user in the system to see if they follow a.
+  If they follow a, the user will unfollow a.
+
+  In the second part of the function, an outer loop will iterate over all the users in the system. When the user intended for deletion is
   matched, then each subsequent user is shifted one place to the left of the user array, which overwrites
   the deleted user.
   The special case is when the user for deletion is the last user in the array. In that case, we don't shift
@@ -13,10 +16,12 @@ void deleteuser(user* a, twitter *system) {
     printf("%s's account has been deleted\n", a -> username);
     printf("next user is automatically activated\n");
 
+    //iterate over every user to see if they follow a. If they do, unfollow a, since a deleted their account
     for (int i = 0; i < system -> num_users; i++) {
         for (int j = 0; j <= system -> users[i] .num_following; j++) {
+          //if they do follow a
             if (strcmp(a -> username, system -> users[i].following[j]) == 0) {
-                unfollow(&system -> users[i], a);
+                unfollow(&system -> users[i], a); //user in iteration will unfollow a
             }
         }
     }
@@ -29,9 +34,10 @@ void deleteuser(user* a, twitter *system) {
 
                 //if deleted user is not last (if it is last just decrement the number of users)
                 if (i < system -> num_users - 1) {
-                    struct user *a = &system->users[j];
+                    //shift all preceding users to the left
+                    struct user *a = &system->users[j]; 
                     struct user *b = &system->users[j + 1];
-                    *a = *b;
+                    *a = *b; //a is replaced by the user that comes after
                 }
             }
         }
